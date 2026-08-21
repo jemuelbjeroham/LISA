a909a6479a4754808b3957aa246c33a1bcdd0a8a
Jemuel B Jeroham <jemuelbjeroham@outlook.com>
2026-08-22 04:43:25 +0530

    feat: Add initial LISA graph with orchestrator node

--- src/lisa/graph.py
+++ src/lisa/graph.py
@@ -1,3 +1,16 @@
+from langgraph.graph import END, START, StateGraph
+
 from lisa.state import LISAState
 
 
+def orchestrator(state: LISAState) -> dict:
+    print("Orchestrator received: ", state["messages"])
+    return {}
+
+builder = StateGraph(LISAState)
+
+builder.add_node("orchestrator", orchestrator)
+builder.add_edge(START, "orchestrator")
+builder.add_edge("orchestrator", END)
+
+graph = builder.compile()
