4529bc9d150049916df4ffbc8dc7240ca1aa96f7
Jemuel B Jeroham <jemuelbjeroham@outlook.com>
2026-08-23 01:50:21 +0530

    feat: Define LISA routing domains

---
+++ src/lisa/routing.py
@@ -0,0 +1,7 @@
+from enum import StrEnum
+
+
+class Route(StrEnum):
+    GENERAL_ENQUIRY = "general_enquiry"
+    ISSUE_REPORTING = "issue_reporting"
+    TECHNICAL_CLARIFICATION = "technical_clarification"
--- src/lisa/state.py
+++ src/lisa/state.py
@@ -2,7 +2,10 @@ from typing import Annotated, TypedDict
 
 from langgraph.graph.message import add_messages
 
+from lisa.routing import Route
+
 
 class LISAState(TypedDict):
     messages: Annotated[list, add_messages]
+    route: Route | None
 
