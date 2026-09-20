9d6280ebcd016dcdc6600851efc29cc2ba381f21
Jemuel B Jeroham <jemuelbjeroham@outlook.com>
2026-09-20 18:45:08 +0530

    feat: Add a fallback model in the config

--- src/lisa/config.py
+++ src/lisa/config.py
@@ -6,8 +6,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="LISA_")
+    #Primary Model
     model_provider: str = "nvidia"
     model_name: str = "nvidia/nemotron-3.5-lightning-30b-a3b"
+    
+    #Fallback Model
+    fallback_model_provider: str = "openai"
+    fallback_model_name: str = "gpt-5-nano"
+
+    #Router Model
     router_model_provider: str = "openai"
     router_model_name: str = "gpt-5-nano"
     openai_token: str = Field(validation_alias="OPENAI_API_KEY")
